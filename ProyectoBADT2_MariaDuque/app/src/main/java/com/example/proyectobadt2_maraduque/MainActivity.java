b5e6a11e7f58ff11421d7911366771cac812ad72
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2023-02-18 20:13:19 +0100

    main done :) consultActivity to go :')

---
+++ ProyectoBADT2_MariaDuque/app/src/main/java/com/example/proyectobadt2_maraduque/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.proyectobadt2_maraduque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnConsult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnConsult = findViewById(R.id.btn_cnslt_earthquake);
+        btnConsult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ConsultActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
