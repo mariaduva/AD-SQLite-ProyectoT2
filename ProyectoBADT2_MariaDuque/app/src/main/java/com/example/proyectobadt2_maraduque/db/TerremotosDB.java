abb308d70c96cc8678f24a471d293e2a51de3ee8
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2023-02-19 13:43:35 +0100

    created TerremotosDB

---
+++ ProyectoBADT2_MariaDuque/app/src/main/java/com/example/proyectobadt2_maraduque/db/TerremotosDB.java
@@ -0,0 +1,30 @@
+package com.example.proyectobadt2_maraduque.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.proyectobadt2_maraduque.dao.PaisesDao;
+import com.example.proyectobadt2_maraduque.dao.TerremotoDao;
+import com.example.proyectobadt2_maraduque.entity.PaisAfectado;
+import com.example.proyectobadt2_maraduque.entity.Terremoto;
+
+@Database(entities = {Terremoto.class, PaisAfectado.class}, version = 1)
+public  abstract class TerremotosDB extends RoomDatabase {
+
+    public abstract TerremotoDao terremotoDao();
+    public abstract PaisesDao paisAfectadoDao();
+
+    private static TerremotosDB terremotoDB;
+
+    public static TerremotosDB getDatabase(Context context) {
+        if (terremotoDB == null) {
+            terremotoDB = Room.databaseBuilder(
+                            context.getApplicationContext(), TerremotosDB.class, "TERREMOTOS_DB")
+                    .allowMainThreadQueries().build();
+        }
+        return terremotoDB;
+    }
+}
