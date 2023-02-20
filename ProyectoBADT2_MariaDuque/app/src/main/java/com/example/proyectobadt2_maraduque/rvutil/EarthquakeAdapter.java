3507dae316b98a6596f6a7dd5564fd6b374f6e39
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2023-02-20 18:38:14 +0100

    Create EarthquakeAdapter.java

---
+++ ProyectoBADT2_MariaDuque/app/src/main/java/com/example/proyectobadt2_maraduque/rvutil/EarthquakeAdapter.java
@@ -0,0 +1,64 @@
+package com.example.proyectobadt2_maraduque.rvutil;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.proyectobadt2_maraduque.R;
+import com.example.proyectobadt2_maraduque.entity.Terremoto;
+
+import java.util.ArrayList;
+
+public class EarthquakeAdapter extends RecyclerView.Adapter<EarthquakeAdapter.EarthquakeVH> {
+
+    private ArrayList<Terremoto> data;
+
+    public EarthquakeAdapter(ArrayList<Terremoto> data){
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public EarthquakeVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.earthquake_item, parent, false);
+        return new EarthquakeVH(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EarthquakeVH holder, int position) {
+        holder.bindData(data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public static class EarthquakeVH extends RecyclerView.ViewHolder{
+        TextView tvName, tvMagnitude, tvPlace, tvDate, tvCoordinates, tvDeaths;
+
+        public EarthquakeVH(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvNameEarthquake);
+            tvMagnitude = itemView.findViewById(R.id.tvMagnitudeEarthquake);
+            tvPlace = itemView.findViewById(R.id.tvPlace);
+            tvDate = itemView.findViewById(R.id.tvDateEarthquake);
+            tvCoordinates = itemView.findViewById(R.id.tvCoordinatesEpi);
+            tvDeaths = itemView.findViewById(R.id.tvDeaths);
+        }
+
+        public void bindData(Terremoto earthquake){
+            tvName.setText(earthquake.nombre);
+            tvMagnitude.setText(String.valueOf(earthquake.magnitud));
+            tvPlace.setText(earthquake.lugar);
+            tvDate.setText(earthquake.fecha);
+            tvCoordinates.setText(earthquake.coordenadas);
+            tvDeaths.setText(earthquake.muertos);
+        }
+    }
+}
